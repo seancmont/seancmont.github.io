7008572803ec70a99906c4191c939d8b83f9c35c
Sean Montgomery <seancmont@gmail.com>
2017-08-15 16:24:51 -0400

    changed styling and layout

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-var tableOffset = $("#table-1").offset().top;
-var $header = $("#table-1 > thead").clone();
-var $fixedHeader = $("#header-fixed").append($header);
-
-$(window).bind("scroll", function() {
-    var offset = $(this).scrollTop();
-    
-    if (offset >= tableOffset && $fixedHeader.is(":hidden")) {
-        $fixedHeader.show();
-    }
-    else if (offset < tableOffset) {
-        $fixedHeader.hide();
+jQuery("#backtotop").click(function () {
+    jQuery("body,html").animate({
+        scrollTop: 0
+    }, 600);
+});
+jQuery(window).scroll(function () {
+    if (jQuery(window).scrollTop() > 150) {
+        jQuery("#backtotop").addClass("visible");
+    } else {
+        jQuery("#backtotop").removeClass("visible");
     }
 });

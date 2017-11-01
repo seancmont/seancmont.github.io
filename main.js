4717507236e0b6fe4d34c78d813158d1c07946e7
Sean Montgomery <seancmont@gmail.com>
2017-10-31 21:12:22 -0400

    changed format, added header, footer

--- main.js
+++ main.js
@@ -1,12 +1,48 @@
-jQuery("#backtotop").click(function () {
-    jQuery("body,html").animate({
-        scrollTop: 0
-    }, 600);
-});
-jQuery(window).scroll(function () {
-    if (jQuery(window).scrollTop() > 150) {
-        jQuery("#backtotop").addClass("visible");
-    } else {
-        jQuery("#backtotop").removeClass("visible");
+/* 
+  Description: Animates the header upon scroll
+  
+*/
+
+$(function(){
+  var header = $("header"),
+      yOffset = 0,
+      triggerPoint = 150;
+  $(window).scroll(function(){
+    yOffset = $(window).scrollTop();
+    
+    if(yOffset >= triggerPoint){
+      header.addClass("minimized");
+    }else{
+      header.removeClass("minimized");
     }
+    
+  });
 });
+
+// Sticky navbar
+
+var headerHeight = 200;
+
+$(window).bind('scroll', function () {
+if ($(window).scrollTop() > headerHeight) {
+    $('#myNav').removeClass('navbar-top');
+    $('#myNav').addClass('navbar-fixed-top');
+} else {
+    $('#myNav').removeClass('navbar-fixed-top');
+    $('#myNav').addClass('navbar-top');
+}
+}); 
+
+// $(document).ready(function() {
+// 		var s = $("#sticker");
+// 		var pos = s.position();					   
+// 		$(window).scroll(function() {
+// 			var windowpos = $(window).scrollTop();
+			
+// 			if (windowpos >= pos.top) {
+// 				s.addClass("stick");
+// 			} else {
+// 				s.removeClass("stick");	
+// 			}
+// 		});
+// 	});

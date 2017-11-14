04abc1cf01953125aa13b4d1408304bea897ff17
Sean Montgomery <seancmont@gmail.com>
2017-11-14 15:04:22 -0500

    added portfolio page with project thumbnail, styling edits and changes to navbar

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// function navFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topNav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topNav";
+//     }
+// }
+
+
 /* 
   Description: Animates the header upon scroll
   
